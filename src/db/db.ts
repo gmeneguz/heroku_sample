fe7a80d78f4076ad0164d6e5f575e005a22ea1df
Gianfranco <gmeneguz@gmail.com>
2021-09-01 19:12:22 -0300

    fix: sequelize connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,8 +4,17 @@ const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
   throw new Error("DATABASE_URL env var not defined");
 }
+let sequelizeOptions: any = {};
+if (process.env.NODE_ENV !== "development") {
+  sequelizeOptions.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  };
+}
 
-export const sequelize = new Sequelize(connectionString);
+export const sequelize = new Sequelize(connectionString, sequelizeOptions);
 
 export async function startDb() {
   try {
